8043e04e395eb445c3d080cfab96a9811d8d7aa4
jan-petrak <jan.petrak31@gmail.com>
2024-12-04 21:46:22 +0100

    day1 impl and also init commit (whops forgot to make it)

---
+++ aoc2024/Day1/Day1.cs
@@ -0,0 +1,28 @@
+﻿namespace aoc2024.Day1;
+
+public static class Day1
+{
+    public static void Solve()
+    {
+        var filePath = Path.GetFullPath(Path.Combine("Day1", "input.txt"), AppContext.BaseDirectory);
+
+        List<int> firstColumn = [];
+        List<int> secondColumn = [];
+
+        foreach (var line in File.ReadLines(filePath))
+        {
+            var row = line.Split([' '], StringSplitOptions.RemoveEmptyEntries);
+
+            firstColumn.Add(int.Parse(row[0]));
+            secondColumn.Add(int.Parse(row[1]));
+        }
+
+        firstColumn.Sort();
+        secondColumn.Sort();
+
+        var result = firstColumn.Zip(secondColumn).Sum(x => Math.Abs(x.First - x.Second));
+
+        Console.WriteLine(result);
+    }
+}
+
---
+++ aoc2024/Program.cs
@@ -0,0 +1,3 @@
+﻿using aoc2024.Day1;
+
+Day1.Solve();
